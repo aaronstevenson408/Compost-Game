cb9ef4e160bcff066d2297b6833dd291ce053356
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2024-04-06 18:31:33 -0700

    tables made tables kept

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -1,5 +1,5 @@
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections.Generic;
+// using UnityEngine;
 
 // public class ItemSpawner : MonoBehaviour
 // {
@@ -32,7 +32,6 @@ using UnityEngine;
 //         return spawnedItems;
 //     }
 // }
-// TODO: implement after prefabs made 
 
 using System.Collections.Generic;
 using UnityEngine;
@@ -42,24 +41,26 @@ public class ItemSpawner : MonoBehaviour
     public List<CompostableItem> itemPrefabs; // A list of CompostableItem prefabs to spawn
     public int minItemsToSpawn = 5; // Minimum number of items to spawn
     public int maxItemsToSpawn = 10; // Maximum number of items to spawn
-
+    public GameObject spawnArea; // Reference to the spawn area (table) GameObject
+    public float spawnAreaRadius = 1f; // Radius of the spawn area around the table
     public List<CompostableItem> SpawnItems()
     {
         List<CompostableItem> spawnedItems = new List<CompostableItem>();
 
         // Determine the number of items to spawn
         int itemCount = Random.Range(minItemsToSpawn, maxItemsToSpawn + 1);
-
         for (int i = 0; i < itemCount; i++)
         {
             // Select a random prefab from the list
             CompostableItem prefab = itemPrefabs[Random.Range(0, itemPrefabs.Count)];
 
-            // Instantiate the prefab and add it to the list
-            CompostableItem spawnedItem = Instantiate(prefab.gameObject).GetComponent<CompostableItem>();
+            // Calculate a random position within the spawn area
+            Vector3 randomPosition = spawnArea.transform.position + Random.insideUnitCircle * spawnAreaRadius;
+
+            // Instantiate the prefab at the random position and add it to the list
+            CompostableItem spawnedItem = Instantiate(prefab.gameObject, randomPosition, Quaternion.identity).GetComponent<CompostableItem>();
             spawnedItems.Add(spawnedItem);
         }
-
         return spawnedItems;
     }
 }
