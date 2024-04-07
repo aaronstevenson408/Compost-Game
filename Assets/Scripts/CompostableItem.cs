766e4726073df57dc0fe174c599f472b40ecf741
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2024-04-06 17:17:40 -0700

    grabbed some assets and created a generator for the item data

--- Assets/Scripts/CompostableItem.cs
+++ Assets/Scripts/CompostableItem.cs
@@ -3,11 +3,13 @@ public class CompostableItem
     public string itemName;
     public float nitrogenContent; // For "green" materials
     public float carbonContent; // For "brown" materials
+    public float itemWeight; // in lbs 
 
-    public CompostableItem(string name, float nitrogen, float carbon)
+    public CompostableItem(string name, float nitrogen, float carbon, float weight)
     {
         itemName = name;
         nitrogenContent = nitrogen;
         carbonContent = carbon;
+        itemWeight = weight;
     }
 }
